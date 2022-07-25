676fbfbcb0edf762d2c1aeb669a39e220f8c326e
shuvam77 <shuvam77@gmail.com>
2022-07-25 20:16:49 +0200

    listing items

--- stock/urls.py
+++ stock/urls.py
@@ -5,4 +5,5 @@ app_name = "stock"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("list_items", views.ListItem.as_view(), name="list_items")
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from stock.models import Stock
 
 # Create your views here.
 
@@ -6,3 +9,18 @@ from django.shortcuts import render
 def home(request):
     something = "Page working!"
     return render(request, "home.html", {"something": something})
+
+
+class ListItem(ListView):
+    model = Stock
+    paginate_by= 10
+    context_object_name = 'stock_list'
+    template_name = 'list_items.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "List of Stock Items"
+        return context
+
+
+
