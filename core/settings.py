4348f17b3ce9bd1f0b91a2c84bfac4fd3b930e43
shuvam77 <shuvam77@gmail.com>
2022-07-31 15:37:28 +0200

    crispy forms

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stock',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- stock/forms.py
+++ stock/forms.py
@@ -1,7 +1,21 @@
 from django import forms
 from .models import Stock
 
+Category = (
+    ('', 'Choose...'),
+    ('CHOCO', 'Chocolate'),
+    ('BEER', 'Beer'),
+    ('WHI', 'Whisky'),
+    ('CHIPS', 'Chips')
+
+)
+
 class StockForm(forms.ModelForm):
+
+    category = forms.ChoiceField(choices=Category)
+    item_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder':'Name'}))
+    quantity = forms.IntegerField(max_value=50, widget=forms.NumberInput(attrs={'placeholder':'Quantity'}))
+
     class Meta:
         model = Stock
         fields = ['category', 'item_name', 'quantity']
